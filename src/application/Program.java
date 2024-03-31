dc6835efd2dbb04de1d813ac2eb788f2fc6c1761
Miguel Nascimento <110462979+ShiMigui@users.noreply.github.com>
2024-03-30 21:57:23 -0300

    Added: class Department and Program(main[])

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department d = new Department(1, "Computers");
+	}
+
+}
---
+++ src/entities/Department.java
@@ -0,0 +1,55 @@
+package entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Department implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+
+	public Department(Integer id, String name) {
+		setId(id);
+		setName(name);
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
+	}
+
+}
